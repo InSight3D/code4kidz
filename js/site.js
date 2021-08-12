81bab509e431769447dc7f0b3b7fa1815dfc190c
Neil Shah <neildshah1013@gmail.com>
2021-08-12 14:35:19 -0400

    fixed navbar stuff

--- js/site.js
+++ js/site.js
@@ -2,13 +2,21 @@ var navLinks = document.getElementById("navLinks");
 
 // Show The Menu (Phone)
 function showMenu(){
-    navLinks.style.right="0";
+    navLinks.style.right="-60px";
 }
 
 // Hide The Menu (Phone)
 function hideMenu(){
     navLinks.style.right = "-200px";
 }
+
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+      navLinks.style.right = "-200px";
+    prevScrollpos = currentScrollPos;
+  }
 
 $('a[href*=\\#]').on('click', function(event){     
     event.preventDefault();
